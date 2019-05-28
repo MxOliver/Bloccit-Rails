8b11cf4880d6d6ded4b9694c610ad602e2660bdb
Oliver Coley <mxoliverleigh@gmail.com>
2019-05-28 15:03:55 -0400

    rewrite every 5th post title as SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
 class PostsController < ApplicationController
   def index
 
-    @posts = Post.all
+    @posts = find_spam(Post.all)
+  end
+
+  def find_spam(posts)
+    posts.each_with_index do |post, index|
+      if post.id % 5 === 0
+        @spam_post = Post.find(post[:id])
+        @spam_post.update(title: "SPAM")
+      else
+        next
+      end
+    end
+
+    return Post.all
+
   end
 
   def show
